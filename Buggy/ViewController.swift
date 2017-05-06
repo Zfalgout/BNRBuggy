0486121876218ecb0ea412d6755ff38132b2747f
Zack Falgout <zackfalgout@Zacks-MacBook-Pro.local>
2017-05-05 23:12:30 -0500

    Updated the code and went through breakpoint exercises.  Odd thing is, my buggy application doesn't crash...It should  though.  It inserts into an array 4 times and removes 12 times.  Very odd.

--- Buggy/ViewController.swift
+++ Buggy/ViewController.swift
@@ -10,14 +10,29 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    @IBAction func buttonTapped(_ sender: UIButton){
+    //@IBAction func switchToggled(_ sender: UISwitch){
+        //print("Called buttonTapped(_:)")
+        print("Method \(#function) in file: \(#file) line: \(#line) called.")
+        //Log sender:
+        //print("sender: \(sender)")
+        //Log the control state.
+        //print("Is control on? \(sender.isOn)")
+        
+        badMethod()
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    func badMethod(){
+        let array = NSMutableArray()
+        
+        for i in 0..<4{
+            array.insert(i, at: i)
+        }
+        
+        //Go one step too far emptying the array (notice the range change):
+        for i in 0...11{
+            array.remove(at: i)
+        }
     }
 
 
